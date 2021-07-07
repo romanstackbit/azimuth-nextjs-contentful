fc51cbb84e15d410c9a4e9f45a9d39a6688a5ca8
Simon Hanukaev <semenh@gmail.com>
2021-07-07 15:38:37 +0300

    updated content-model

--- contentful/export.js
+++ contentful/export.js
@@ -4,6 +4,7 @@ const contentfulExport = require('contentful-export');
 
 const managementToken = process.env.CONTENTFUL_MANAGEMENT_TOKEN || process.argv[2];
 const spaceId = process.env.CONTENTFUL_SPACE_ID || process.argv[3];
+const environmentId = process.env.CONTENTFUL_ENVIRONMENT || process.argv[4] || 'master';
 
 if (!managementToken || !spaceId) {
     console.error('Contentful management token or space ID were not provided.\n\nUsage:\n./export.js <managementToken> <spaceId>\n');
@@ -12,6 +13,7 @@ if (!managementToken || !spaceId) {
 
 const options = {
     spaceId: spaceId,
+    environmentId: environmentId,
     managementToken: managementToken,
     exportDir: __dirname,
     contentFile: 'export.json',
